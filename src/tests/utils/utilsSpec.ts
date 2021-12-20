b47f82046700b8ead030a4debbe7ad3f23632300
Mostafa Said <mostafa.said.ext@orange.com>
2021-12-20 19:31:54 +0200

    Fix bugs

---
+++ src/tests/utils/utilsSpec.ts
@@ -0,0 +1,21 @@
+import { processImage } from '../../utils/utils';
+import fs from 'fs';
+import path from 'path';
+
+describe('Test endpoint responses', () => {
+  it('Test create iamge', async () => {
+    const basePath = `${__dirname}/../../../images`;
+    const temp = `${basePath}/thumb/fjord-150-150.jpg`;
+
+    const checkFile = (temp: string): boolean => {
+      if (fs.existsSync(path.normalize(temp))) return true;
+
+      return false;
+    };
+
+    return await processImage(basePath, 'fjord', 150, 150).then((result) => {
+      console.log(result);
+      expect(checkFile(temp)).toEqual(true);
+    });
+  });
+});
